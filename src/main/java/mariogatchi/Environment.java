9de4159f52573017f81967b620745ff57b42cf83
Nicole Leung <nxin.leung@mail.utoronto.ca>
2022-10-28 17:46:22 -0400

    Test

---
+++ src/main/java/mariogatchi/Environment.java
@@ -0,0 +1,9 @@
+package mariogatchi;
+
+import java.util.ArrayList;
+
+public class Environment {
+    private ArrayList<Actions> eligibleActions;
+
+
+}
---
+++ src/main/java/mariogatchi/Forest.java
@@ -0,0 +1,10 @@
+package mariogatchi;
+
+import java.util.ArrayList;
+
+public class Forest extends Environment{
+    private ArrayList<Actions> eligibleActions;
+
+    public ArrayList<>
+}
+
