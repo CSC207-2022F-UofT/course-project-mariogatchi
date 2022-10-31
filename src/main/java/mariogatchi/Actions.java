bbf32e5f9ae6892c161dba7646ba933f92a14c7c
Nicole Leung <nxin.leung@mail.utoronto.ca>
2022-10-31 13:56:59 -0400

    Actions class and nevermind, release interfaces

--- src/main/java/mariogatchi/Actions.java
+++ src/main/java/mariogatchi/Actions.java
@@ -1,24 +1,17 @@
 package mariogatchi;
 
-public class Actions {
+public class Actions implements Nevermind, Release{
+    /**
+     * Class that implements the interfaces.
+     **/
 
-    public String releaseMariogatchi(){
-        /**
-         * Releases the selected Mariogatchi from the Users collection of Mariogatchi.
-         * Deletes the selected Mariogatchi's statistics.
-         *
-         * @param userName the username
-         * @param password the passwor
-         * TODO: Remove the Mariogatchi statistics, remove Mariogatchi from collection of Mariogatchis
-         * TODO: Return the User to view main list of eligibleActions of Forest
-         */
-        return "Mariogatchi has been released.";
+    @Override
+    public String nevermindRelease(){
+        return "Mariogatchi remains in your inventory.";
     }
 
-    public String neverMind(){
-        /*
-          TODO: Return the User to view main list of eligibleActions of Forest
-         */
-        return "Mariogatchi remains in your inventory.";
+    @Override
+    public String releaseMariogatchi() {
+        return "Mariogatchi has been released.";
     }
 }
---
+++ src/main/java/mariogatchi/Nevermind.java
@@ -0,0 +1,8 @@
+package mariogatchi;
+
+public interface Nevermind {
+    /**
+     * Releases the selected Mariogatchi from the Users collection of Mariogatchi.
+     **/
+    String nevermindRelease();
+}
