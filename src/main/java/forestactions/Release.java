54e0eb57f9e6327bc8a48e0c117bfc6727930cf3
Nicole Leung <nxin.leung@mail.utoronto.ca>
2022-11-17 21:09:36 -0500

    Release class: placeholder for release implementation TODO

---
+++ src/main/java/forestactions/Release.java
@@ -0,0 +1,11 @@
+package forestactions;
+
+//releasing a specified mariogatchi from the users collection of mariogatchi's
+public class Release implements ForestAction {
+
+    //TODO: implementation of the release method, todo myself
+    @Override
+    public void executes() {
+        System.out.println("release");
+    }
+}
