58c6e9a112fc87b559c170b26ad4477c03438312
Nicole Leung <nxin.leung@mail.utoronto.ca>
2022-11-17 21:15:40 -0500

    CatchDemo class: placeholder for CatchDemo implementation

---
+++ src/main/java/forestactions/CatchDemo.java
@@ -0,0 +1,11 @@
+package forestactions;
+
+//catches the mariogatchi
+public class CatchDemo implements ForestAction{
+
+    //TODO: implementation of the catch method, ask Najim
+    @Override
+    public void executes() {
+        System.out.println("catch");
+    }
+}
