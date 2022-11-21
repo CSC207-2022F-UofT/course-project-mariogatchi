0c6f829e95bf7be9217ee9f214651609e9f87a6d
Nicole Leung <nxin.leung@mail.utoronto.ca>
2022-11-21 01:35:56 -0500

    Implemented use case where mariogatchi is removed by illness

---
+++ src/main/java/mariogatchi/removemariogatchi/killmariogatchi/Illness.java
@@ -0,0 +1,23 @@
+package mariogatchi.removemariogatchi.killmariogatchi;
+
+import mariogatchi.Mariogatchi;
+import mariogatchi.RemoveMariogatchiPresenterInterface;
+import mariogatchi.RemoveMariogatchiPresenter;
+import mariogatchi.User;
+
+/**
+ * Use case
+ * Event where a Mariogatchi dies from illness
+ */
+public class Illness extends KillMariogatchi{
+
+    @Override
+    public void execute(User user, Mariogatchi mario, RemoveMariogatchiPresenter presenter) {
+        if (Kill(user, mario)){
+            presenter.getKillActionType(RemoveMariogatchiPresenterInterface.KillAction.ILLNESS);
+        }
+        else {
+            presenter.getKillActionType(RemoveMariogatchiPresenterInterface.KillAction.ERROR);
+        }
+    }
+}
