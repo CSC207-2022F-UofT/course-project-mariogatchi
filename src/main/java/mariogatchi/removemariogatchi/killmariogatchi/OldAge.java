859b3fd9d8e47b72c3fa52b4245cc8ba1236f818
Nicole Leung <nxin.leung@mail.utoronto.ca>
2022-11-21 01:37:29 -0500

    Implemented use case where mariogatchi is removed by old age

---
+++ src/main/java/mariogatchi/removemariogatchi/killmariogatchi/OldAge.java
@@ -0,0 +1,23 @@
+package mariogatchi.removemariogatchi.killmariogatchi;
+
+import mariogatchi.Mariogatchi;
+import mariogatchi.RemoveMariogatchiPresenterInterface;
+import mariogatchi.RemoveMariogatchiPresenter;
+import mariogatchi.User;
+
+/**
+ * Use case
+ * Event where a Mariogatchi dies by Old Age
+ */
+public class OldAge extends KillMariogatchi{
+
+    @Override
+    public void execute(User user, Mariogatchi mario, RemoveMariogatchiPresenter presenter) {
+        if (Kill(user, mario)){
+            presenter.getKillActionType(RemoveMariogatchiPresenterInterface.KillAction.OLDAGE);
+        }
+        else {
+            presenter.getKillActionType(RemoveMariogatchiPresenterInterface.KillAction.ERROR);
+        }
+    }
+}
