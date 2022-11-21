834cfddd5a482e8a60d5393ae12067ea52ced5d6
Nicole Leung <nxin.leung@mail.utoronto.ca>
2022-11-21 00:55:08 -0500

    Implemented RemoveMariogatchiPresenterInterface Input boundary for all actions for removing a Mariogatchi

---
+++ src/main/java/mariogatchi/RemoveMariogatchiPresenterInterface.java
@@ -0,0 +1,33 @@
+package mariogatchi;
+
+/**
+ * Input boundary
+ * All actions for removing a Mariogatchi
+ */
+public interface RemoveMariogatchiPresenterInterface {
+    String getKillActionType(KillAction type);
+    String getTransferActionType(TransferAction type);
+
+    /**
+     * The types of kill actions for a Mariogatchi entity
+     */
+    enum KillAction {
+        BLOWNAWAY,
+        DROWN,
+        ILLNESS,
+        OLDAGE,
+        OVERCARE,
+        PREDATORS,
+        PROBLEMSET,
+        STARVATION,
+        UFOABDUCTION
+    }
+
+    /**
+     * The types of transfer actions for a Mariogatchi entity
+     */
+    enum TransferAction{
+        RELEASE,
+        MERGE
+    }
+}
