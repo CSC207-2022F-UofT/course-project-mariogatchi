105154a091b169f27c7cd5b87f7332d81fccf283
Nicole Leung <nxin.leung@mail.utoronto.ca>
2022-11-21 02:14:15 -0500

    testing the removal feature

--- src/main/java/mariogatchi/Main.java
+++ src/main/java/mariogatchi/Main.java
@@ -1,7 +1,28 @@
 package mariogatchi;
 
-import mariogatchi.removemariogatchi.RemoveMariogatchi;
-import mariogatchi.removemariogatchi.killmariogatchi.Starvation;
+import mariogatchi.removemariogatchi.factories.*;
+
+import java.util.ArrayList;
 
 public class Main {
+    public static void main(String[] args) {
+        User user= new User("test user");
+
+        Mariogatchi mariogatchi = new Mariogatchi();
+        Mariogatchi mario = new Mariogatchi();
+
+        MariogatchiManager manager = new MariogatchiManager();
+        user.setMariogatchis(new ArrayList<>());
+
+        manager.AddMariogatchi(user, mariogatchi);
+        manager.AddMariogatchi(user, mario);
+
+        RemoveMariogatchiPresenter presenter = new RemoveMariogatchiPresenter();
+        RemoveMariogatchiDisplayer displayer = new RemoveMariogatchiDisplayer();
+
+        RemoveMariogatchiFactory transferFactory = new TransferMariogatchiFactory();
+        RemoveMariogatchiFactory killFactory = new KillMariogatchiFactory();
+
+        killFactory.getAction("illness").execute(user, mariogatchi, presenter, displayer);
+    }
 }
