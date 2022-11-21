0708a3a3ddfdf7a588f570167d28bd0d5e165c26
Nicole Leung <nxin.leung@mail.utoronto.ca>
2022-11-21 00:51:07 -0500

    Implemented Concrete factory for cases to transfer a Mariogatchi: TransferMariogatchiFactory

---
+++ src/main/java/mariogatchi/removemariogatchi/factories/TransferMariogatchiFactory.java
@@ -0,0 +1,30 @@
+package mariogatchi.removemariogatchi.factories;
+
+import mariogatchi.removemariogatchi.transfermariogatchi.Merge;
+import mariogatchi.removemariogatchi.transfermariogatchi.Release;
+import mariogatchi.removemariogatchi.transfermariogatchi.TransferMariogatchi;
+
+/**
+ * Concrete factory for cases to transfer a Mariogatchi
+ */
+public class TransferMariogatchiFactory implements RemoveMariogatchiFactory {
+
+    /**
+     * uses getAction method to get the specified use case to transfer a Mariogatchi
+     *
+     * @param Action inputs a transfer action
+     * @return use case for the specified action, otherwise return null if the action cannot be specified
+     */
+    public TransferMariogatchi getAction(String Action) {
+        switch (Action) {
+            case "release":
+                return new Release();
+            case "merge":
+                return new Merge();
+
+            default:
+                System.out.println("Action " + Action + " cannot be specified and does not run.");
+                return null;
+        }
+    }
+}
