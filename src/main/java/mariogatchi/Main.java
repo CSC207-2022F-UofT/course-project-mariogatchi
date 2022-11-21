a63423985981fd5a7f5b6db58d5e120986d46716
Nicole Leung <nxin.leung@mail.utoronto.ca>
2022-11-21 01:04:40 -0500

    Test file, deleted code to add another test later

--- src/main/java/mariogatchi/Main.java
+++ src/main/java/mariogatchi/Main.java
@@ -4,11 +4,4 @@ import mariogatchi.removemariogatchi.RemoveMariogatchi;
 import mariogatchi.removemariogatchi.killmariogatchi.Starvation;
 
 public class Main {
-    public static void main(String[] args) {
-        RemoveMariogatchi r = new Starvation();
-        User user= new User("your mom");
-        ReleasePresenter presenter = new ReleasePresenter();
-        Mariogatchi mario = new Mariogatchi();
-        r.execute(user, mario, presenter);
-    }
 }
