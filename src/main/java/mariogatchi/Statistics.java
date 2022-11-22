1f20976085b94e2dbcb938f1c7f3b6bf5b5e7288
vivek <68870568+vivekvzdsw@users.noreply.github.com>
2022-11-21 22:33:08 -0500

    Made changeStat and added ENUMs

--- src/main/java/mariogatchi/Statistics.java
+++ src/main/java/mariogatchi/Statistics.java
@@ -38,7 +38,21 @@ public class Statistics implements Serializable {
      * The last time this Mariogatchi's stats were checked.
      */
     private long lastCheckTime;
-
+    
+    
+    enum Operator{
+         ADD,
+         SUBTRACT
+    }
+     enum Stats{
+         HUNGER,
+         ENERGY,
+         CLEANLINESS,
+         HAPPINESS,
+         AGILITY,
+         STRATEGY,
+         LASTCHECKTIME
+    }
 
     /**
      * Initiate Stats for a new Mariogatchi.
@@ -89,142 +103,100 @@ public class Statistics implements Serializable {
         return this.lastCheckTime;
     }
 
-    public void reduceStat(String stat, int factor){
+    public boolean changeStat(Stats stat, int factor, Operator operate){
         /**
          * make sure to check for string errors
          */
-
-        switch (stat.toLowerCase()){//use switch case for efficiency
-            case "hunger":
-                if(this.hunger >=(factor))
-                    this.hunger-=(factor);
-                break;
-            case "energy":
-                if(this.energy >=(factor))
-                    this.energy-=(factor);
-                break;
-            case "cleanliness":
-                if(this.cleanliness >=(factor))
-                    this.cleanliness-=(factor);
-                break;
-            case "happiness":
-                if(this.happiness >=(factor))
-                    this.happiness-=(factor);
-                break;
-            case "agility":
-                if(this.agility >=(factor))
-                    this.agility-=(factor);
-                break;
-            case "strategy":
-                if(this.strategy >=(factor))
-                    this.strategy-=(factor);
-                break;
+        boolean changed = true;
+        if(operate == Operator.SUBTRACT && factor > 0) {
+            switch (stat) {//use switch case for efficiency
+                case HUNGER:
+                    if (this.hunger >= (factor))
+                        this.hunger -= (factor);
+                    break;
+                case ENERGY:
+                    if (this.energy >= (factor))
+                        this.energy -= (factor);
+                    break;
+                case CLEANLINESS:
+                    if (this.cleanliness >= (factor))
+                        this.cleanliness -= (factor);
+                    break;
+                case HAPPINESS:
+                    if (this.happiness >= (factor))
+                        this.happiness -= (factor);
+                    break;
+                case AGILITY:
+                    if (this.agility >= (factor))
+                        this.agility -= (factor);
+                    break;
+                case STRATEGY:
+                    if (this.strategy >= (factor))
+                        this.strategy -= (factor);
+                    break;
+            }
         }
+        if(operate == Operator.SUBTRACT && factor > 0){
+            switch (stat){
+                case HUNGER:
+                    if(this.hunger<=max-((factor)))
+                        this.hunger+=(factor);
+                    else
+                        this.hunger =max;
+                    break;
+                case ENERGY:
+                    if(this.energy<=max-((factor)))
+                        this.energy+=(factor);
+                    else
+                        this.energy =max;
+                    break;
+                case CLEANLINESS:
+                    if(this.cleanliness<=max-((factor)))
+                        this.cleanliness+=(factor);
+                    else
+                        this.cleanliness = max;
+                    break;
+                case HAPPINESS:
+                    if(this.happiness <= max - ((factor)))
+                        this.happiness += (factor);
+                    else
+                        this.happiness = max;
+                    break;
+                case AGILITY:
+                    if(this.agility<=max-((factor)))
+                        this.agility+=(factor);
+                    else
+                        this.agility =max;
+                    break;
+                case STRATEGY:
+                    if(this.strategy<=max-((factor)))
+                        this.strategy+=(factor);
+                    else
+                        this.strategy = max;
+                    break;
+            }
+        }
+        else
+            changed = false;
 
+
+        return changed;
     }
 
-    public void increaseStat(String stat, int factor){
-        /**
-         * make sure to check for string errors
-         */
-        switch (stat.toLowerCase()){
-            case "hunger":
-                if(this.hunger<=maxLevel-((factor)))
-                    this.hunger+=(factor);
-                else
-                    this.hunger =maxLevel;
-                break;
-            case "energy":
-                if(this.energy<=maxLevel-((factor)))
-                    this.energy+=(factor);
-                else
-                    this.energy =maxLevel;
-                break;
-            case "cleanliness":
-                if(this.cleanliness<=maxLevel-((factor)))
-                    this.cleanliness+=(factor);
-                else
-                    this.cleanliness = maxLevel;
-                break;
-            case "happiness":
-                if(this.happiness <= maxLevel - ((factor)))
-                    this.happiness += (factor);
-                else
-                    this.happiness = maxLevel;
-                break;
-            case "agility":
-                if(this.agility<=maxLevel-((factor)))
-                    this.agility+=(factor);
-                else
-                    this.agility =maxLevel;
-                break;
-            case "strategy":
-                if(this.strategy<=maxLevel-((factor)))
-                    this.strategy+=(factor);
-                else
-                    this.strategy = maxLevel;
-                break;
-        }
-    }
-
-    public int getStat(String stat){
-        return switch (stat.toLowerCase()) {
-            case "hunger" -> this.hunger;
-            case "energy" -> this.energy;
-            case "cleanliness" -> this.cleanliness;
-            case "happiness" -> this.happiness;
-            case "agility" -> this.agility;
-            case "strategy" -> this.strategy;
+    public int getStat(Stats stat){
+        return switch (stat) {
+            case HUNGER -> this.hunger;
+            case ENERGY-> this.energy;
+            case CLEANLINESS-> this.cleanliness;
+            case HAPPINESS -> this.happiness;
+            case AGILITY -> this.agility;
+            case STRATEGY -> this.strategy;
             default -> 0;
         };
 
     }
 
-    public void statChange(String func){
-
-        switch(func.toLowerCase()){
-            case "decay":
-                reduceStat("hunger",1);
-                reduceStat("energy", 1);
-                reduceStat("cleanliness", 1);
-                reduceStat("happiness", 1);
-                reduceStat("agility", 1);
-                reduceStat("strategy", 1);
-                break;
-            case "feed":
-                increaseStat("happiness", 1);
-                increaseStat("energy", 1);
-                increaseStat("hunger", 2);
-
-            case "bathe":
-                increaseStat("happiness", 1);
-                increaseStat("cleanliness", 1);
-
-                break;
-            case "walk":
-                increaseStat("happiness", 1);
-                reduceStat("energy", 1);
-                reduceStat("cleanliness", 1);
-                break;
-            case "sleep":
-                increaseStat("happiness", 2);
-                this.energy = maxLevel;
-                reduceStat("cleanliness", 1);
-                reduceStat("hunger", 1);
-                break;
-            case "train":
-                increaseStat("agility", 1);
-                increaseStat("strategy", 1);
-                reduceStat("energy",1 );
-                reduceStat("cleanliness", 1);
-                break;
-            case "play":
-                increaseStat("happiness", 1);
-                reduceStat("energy", 1);
-                break;
-        }
-    }
-
+   
     /**
      * Decay mariogatchi's stats due to passed time.
      * @param currentTime - current time as millis long
@@ -236,10 +208,10 @@ public class Statistics implements Serializable {
         long intervals30min = elapsedTime / (30 * 60 * 1000);
         // decrement stats by 2 for each hour (1 per 30min)
         int reduceBy =  (int) intervals30min;
-        reduceStat("hunger", reduceBy);
-        reduceStat("energy", reduceBy);
-        reduceStat("cleanliness", reduceBy);
-        reduceStat("happiness", reduceBy);
+        changeStat(Stats.HUNGER, reduceBy, Operator.SUBTRACT);
+        changeStat(Stats.ENERGY, reduceBy, Operator.SUBTRACT);
+        changeStat(Stats.CLEANLINESS, reduceBy, Operator.SUBTRACT);
+        changeStat(Stats.HAPPINESS, reduceBy, Operator.SUBTRACT);
         this.lastCheckTime = currentTime;
         return elapsedTime;
     }
