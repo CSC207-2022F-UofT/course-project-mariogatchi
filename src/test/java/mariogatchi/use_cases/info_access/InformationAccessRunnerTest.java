631814f13be10aa0e4977a496dc6ce473492dfff
Anthony DiMaggio <42413529+adimaggio2021@users.noreply.github.com>
2022-12-08 18:16:09 -0500

    Update InformationAccessRunnerTest.java

--- src/test/java/mariogatchi/use_cases/info_access/InformationAccessRunnerTest.java
+++ src/test/java/mariogatchi/use_cases/info_access/InformationAccessRunnerTest.java
@@ -1,9 +1,11 @@
 package mariogatchi.use_cases.info_access;
 
-import mariogatchi.entities.Statistics;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import mariogatchi.entities.Mariogatchi;
+import mariogatchi.entities.User;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,7 +16,7 @@ class InformationAccessRunnerTest {
         InformationAccessRunner infoAccessRunner = new InformationAccessRunner(new InfoAccessPresenter() {
             @Override
             public InfoAccessResponseModel prepareSuccessView(InfoAccessResponseModel responseModel) {
-                assertEquals(50, responseModel.getCurrentValue());
+                assertEquals(50, responseModel.getStats().getHunger());
                 return null;
             }
 
@@ -24,7 +26,12 @@ class InformationAccessRunnerTest {
             }
         });
 
-        infoAccessRunner.checkStatistic(new InfoAccessRequestModel(new Statistics("common"),
-                Statistics.Stats.AGILITY.toString()));
+        User u = new User("");
+        List<Mariogatchi> lst = new ArrayList<Mariogatchi>();
+        Mariogatchi m = new Mariogatchi("common");
+        m.setName("J");
+        lst.add(m);
+        u.setMariogatchis(lst);
+        infoAccessRunner.checkStatistic(new InfoAccessRequestModel("J", u));
     }
 }
