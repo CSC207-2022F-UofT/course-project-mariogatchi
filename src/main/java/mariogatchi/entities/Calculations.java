f65037a1e8ca02e94aff05c3d0af750cefac32f8
vivek <68870568+vivekvzdsw@users.noreply.github.com>
2022-12-08 10:02:32 -0500

    Sorting both ways changed
    Made forwards and reverse sorting of the Mariogatchi list

--- src/main/java/mariogatchi/entities/Calculations.java
+++ src/main/java/mariogatchi/entities/Calculations.java
@@ -4,7 +4,10 @@ import java.util.*;
 
 
 public class Calculations {
-
+    public enum sortOrder{
+        FORWARDS,
+        REVERSE
+    }
     public float healthIndex(Mariogatchi mariogatchi1) {
         /**
          * Calculates the health index of mariogatchi which shows how healthy he/she is overall
@@ -71,40 +74,71 @@ public class Calculations {
         return averagee;
     }
 
-    public void sortMariogatchis(List<Mariogatchi> mariogatchiList, String compareBy) {
+    public List<Mariogatchi> sortMariogatchis(List<Mariogatchi> mariogatchiList, String compareBy, sortOrder order) {
 
         /**
          * Sorting Method for mariogatchis
          * Takes the mariogatchi list input and compareby string as the input
          */
-        Collections.sort(mariogatchiList, new Comparator<Mariogatchi>() {
-            public int compare(Mariogatchi m1, Mariogatchi m2) {
-                switch (compareBy) {
-                    case "Agility":
-                        return m1.getStats().getAgility() - m2.getStats().getAgility();
-                    case "Strategy":
-                        return m1.getStats().getStrategy() - m2.getStats().getStrategy();
-                    case "Cleanliness":
-                        return m1.getStats().getCleanliness() - m2.getStats().getCleanliness();
-                    case "Energy":
-                        return m1.getStats().getEnergy() - m2.getStats().getEnergy();
-                    case "Hunger":
-                        return m1.getStats().getHunger() - m2.getStats().getHunger();
-                    case "Happiness":
-                        return m1.getStats().getHappiness() - m2.getStats().getHappiness();
-                    case "Combat":
-                        return (int) (combatIndex(m1) - combatIndex(m2));
-                    case "Health":
-                        return (int) (healthIndex(m1) - healthIndex(m2));
-                    case "Level":
-                    default:
-                        return m1.getLvl() - m2.getLvl();
-                }
+        List<Mariogatchi> returnList = new ArrayList<Mariogatchi>();
+        returnList.addAll(mariogatchiList);
+        switch (order) {
+            case FORWARDS:
+                Collections.sort(returnList, new Comparator<Mariogatchi>() {
+                    public int compare(Mariogatchi m1, Mariogatchi m2) {
+                        switch (compareBy) {
+                            case "Agility":
+                                return m1.getStats().getAgility() - m2.getStats().getAgility();
+                            case "Strategy":
+                                return m1.getStats().getStrategy() - m2.getStats().getStrategy();
+                            case "Cleanliness":
+                                return m1.getStats().getCleanliness() - m2.getStats().getCleanliness();
+                            case "Energy":
+                                return m1.getStats().getEnergy() - m2.getStats().getEnergy();
+                            case "Hunger":
+                                return m1.getStats().getHunger() - m2.getStats().getHunger();
+                            case "Happiness":
+                                return m1.getStats().getHappiness() - m2.getStats().getHappiness();
+                            case "Combat":
+                                return (int) (combatIndex(m1) - combatIndex(m2));
+                            case "Health":
+                                return (int) (healthIndex(m1) - healthIndex(m2));
+                            case "Level":
+                            default:
+                                return m1.getLvl() - m2.getLvl();
+                        }
+                    }
+                });
+            case REVERSE:
+                Collections.sort(returnList, new Comparator<Mariogatchi>() {
+                    public int compare(Mariogatchi m1, Mariogatchi m2) {
+                        switch (compareBy) {
+                            case "Agility":
+                                return m2.getStats().getAgility() - m1.getStats().getAgility();
+                            case "Strategy":
+                                return m2.getStats().getStrategy() - m1.getStats().getStrategy();
+                            case "Cleanliness":
+                                return m2.getStats().getCleanliness() - m1.getStats().getCleanliness();
+                            case "Energy":
+                                return m2.getStats().getEnergy() - m1.getStats().getEnergy();
+                            case "Hunger":
+                                return m2.getStats().getHunger() - m1.getStats().getHunger();
+                            case "Happiness":
+                                return m2.getStats().getHappiness() - m1.getStats().getHappiness();
+                            case "Combat":
+                                return (int) (combatIndex(m2) - combatIndex(m1));
+                            case "Health":
+                                return (int) (healthIndex(m2) - healthIndex(m1));
+                            case "Level":
+                            default:
+                                return m2.getLvl() - m1.getLvl();
+                        }
+                    }
+                });
+        }
 
-
-            }
-        });
-
+        return returnList;
+    }
 
     }
-}
+
